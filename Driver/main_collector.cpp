1368cac2295fa33042dc0ddd7f78e59204216b02
kuba <jaqbsikora@gmail.com>
2021-11-24 12:05:22 +0100

    Filenames are date dependent

--- Driver/main_collector.cpp
+++ Driver/main_collector.cpp
@@ -1,5 +1,7 @@
 #include <thread>
 #include <iostream>
+#include <sstream>
+#include <iomanip>
 #include <mutex>
 #include <string>
 #include <chrono>
@@ -12,15 +14,16 @@
 #define EMG1 2
 #define EMG2 3
 
-
+std::string ekg_filename;
+std::string rest_filename;
 
 
 int main_loop(){
     std::cout<<"Collecting data... type q to stop\n";
     std::thread InputListen(listen_on_stdin);
     while(run){
-        file_ekg.open("thread_test_ekg.txt", std::ios::app);
-        file_rest.open("thread_test_rest.txt", std::ios::app);
+        file_ekg.open(ekg_filename, std::ios::app);
+        file_rest.open(rest_filename, std::ios::app);
         if(!file_ekg.is_open() || !file_rest.is_open()){
             run = false;
             InputListen.join();
@@ -37,12 +40,19 @@ int main_loop(){
     return 0;
 }
 
+void create_files(){
+    auto now  =  std::chrono::system_clock::now();
+    auto in_time_t = std::chrono::system_clock::to_time_t(now);
+    std::stringstream ekg_filestream;
+    std::stringstream rest_filestream;
+    ekg_filestream<<std::put_time()
+}
 
 int main(){
     adc.setGain(GAIN_FOUR);
     adc.begin();
-    file_ekg.open("thread_test_ekg.txt", std::ios::out);
-    file_rest.open("thread_test_rest.txt", std::ios::out);
+    file_ekg.open(ekg_filename, std::ios::out);
+    file_rest.open(rest_filename, std::ios::out);
     file_ekg.close();
     file_rest.close();
     std::srand(std::time(nullptr));
